a42e905091a0111082e511f557fdba1c70051019
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-10-30 21:13:29 -0700

    created a header for urls

--- express_server.js
+++ express_server.js
@@ -8,20 +8,17 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  const templateVars = { id: req.params.id, longURL: longURL};
+  res.render("urls_show", templateVars);
 });
